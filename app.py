9cebc268bb419c1fc6d5e44c6e5f7fc7df32a811
Sowmya Srinivasan <sowmyasrinivasan@berkeley.edu>
2020-07-21 11:22:19 -0700

    first

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import predict_gender
+
+app = Flask(__name__)
+@app.route('/', methods=['post', 'get'])
+def login():
+    message = ''
+    text = ''
+    nb, vecf, sw = predict_gender.init()
+    if request.method == 'POST':
+        text = request.form.get('gendered')
+        print('line 9') 
+        gender = predict_gender.predict_gender(text, nb, vecf, sw)
+        if gender == 'Female':
+            message = "Girl"
+        else:
+            message = "Boy"
+ 
+    return render_template('index.html', message=message, txt=text)
+#...
+@app.route('/data_cleaning.html')
+def source():
+    return render_template('data_cleaning.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+
